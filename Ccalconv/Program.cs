f9e99c1385526e70b42cc69b168eb5c55a1ec6c4
Makoto Tanabe <xekid78@outlook.com>
2018-03-29 10:53:26 +0900

    Add files via upload

---
+++ Ccalconv/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ccalconv
+{
+    class Ccalconv
+    {
+        static void Main(string[] args)
+        {
+            for (int seireki = 1926; seireki <= 1935; seireki++)
+            {
+                Console.Write("西暦" + seireki + "年は");
+                int syowa = seireki - 1925;
+                Console.WriteLine("昭和" + syowa + "年です");
+            }
+        }
+    }
+}
