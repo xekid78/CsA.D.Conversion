9346ce119f59d280d99e49a50d5f4826d216b51f
Makoto Tanabe <xekid78@outlook.com>
2018-04-07 21:29:17 +0900

    for文の中にあったintを外に記述するように変更

--- Ccalconv/Program.cs
+++ Ccalconv/Program.cs
@@ -6,10 +6,11 @@ namespace Ccalconv
     {
         static void Main(string[] args)
         {
+            int syowa;
             for (int seireki = 1926; seireki <= 1935; seireki++)
             {
                 Console.Write("西暦" + seireki + "年は");
-                int syowa = seireki - 1925;
+                syowa = seireki - 1925;
                 Console.WriteLine("昭和" + syowa + "年です");
             }
         }
